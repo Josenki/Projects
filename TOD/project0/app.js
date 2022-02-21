bc99fc346451c1021bfaa221e258d5253f2cc90f
Josenki <98068979+Josenki@users.noreply.github.com>
2022-02-21 14:29:59 -0500

    Add files via upload

---
+++ TOD/project0/app.js
@@ -0,0 +1,15 @@
+document.write("<h1> hello wrodl</h1>"
+);
+
+//tipos de datos 
+"string" 
+'string'
+//string
+
+522222 // number operation 
+
+true //boolean
+
+
+//array
+['jose' , 'nata' , 'vianeth']
